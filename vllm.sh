8b8c0228484d0b956e54ba2f49fa022bfe4953ba
Lucien Thomas <lucienthomas00@gmail.com>
2025-06-09 15:31:57 -0500

    fix

--- vllm.sh
+++ vllm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export OPENAI_API_KEY=asdf
-CUDA_VISIBLE_DEVICES=0 uv run vf-vllm --model 'Qwen/Qwen3-14B' --host 0.0.0.0 --port 8000
+CUDA_VISIBLE_DEVICES=0 uv run vf-vllm --model 'Qwen/Qwen3-14B' --port 8000

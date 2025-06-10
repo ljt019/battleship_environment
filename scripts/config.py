65b16195196eeab70155e92c6d94359b1858f9af
Lucien Thomas <lucienthomas00@gmail.com>
2025-06-10 00:37:21 -0500

    less turns more times

--- scripts/config.py
+++ scripts/config.py
@@ -18,7 +18,7 @@ GRPO_RUN_NAME = f"battleship-grpo-{MODEL_SIZE}"
 # Battleship Game Environment
 # ----------------------------
 
-MAX_TURNS = 40  
+MAX_TURNS = 25  
 SEED = 42 
 
 # ----------------------------
@@ -52,7 +52,7 @@ SFT_OUTPUT_DIR = "sft-battleship"
 # SFT Dataset Generation
 # ----------------------------
 
-NUM_DATASET_SAMPLES = 100  
+NUM_DATASET_SAMPLES = 300  
 DATASET_PATH = "datasets/battleship-sft"  
 HUB_DATASET_NAME = "ljt019/battleship-sft"  
 

3425d96b98ecff63ec098b6123131cbfb294c950
Lucien Thomas <lucienthomas00@gmail.com>
2025-06-11 04:00:24 -0500

    test

--- scripts/train_sft_warmup.py
+++ scripts/train_sft_warmup.py
@@ -9,8 +9,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from scripts.config import MODEL_SIZE, BASE_MODEL_NAME, LEARNING_RATE, NUM_TRAIN_EPOCHS, BATCH_SIZE, SFT_GRADIENT_ACCUMULATION_STEPS, MAX_COMPLETION_LENGTH, SFT_OUTPUT_DIR
 
 model, tokenizer = vf.get_model_and_tokenizer(BASE_MODEL_NAME, use_liger=False)
-model = model.to('cuda')
-dataset = load_dataset('ljt019/battleship-sft', split='train')
+dataset = load_dataset('ljt019/battleship-sft-cleaned', split='train')
 
 # split top 500 highest reward samples for warmup 
 dataset = dataset.sort("reward", reverse=True)

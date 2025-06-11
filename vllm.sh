0d6aba6a2e6f5d334678bebd587581f2b49176a2
Lucien Thomas <lucienthomas00@gmail.com>
2025-06-11 04:08:15 -0500

    hehe

--- vllm.sh
+++ vllm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 uv run vf-vllm --model 'willcb/Qwen3-1.7B' --port 8000 --max-model-len 17336
+CUDA_VISIBLE_DEVICES=0 uv run vf-vllm --model 'ljt019/Qwen3-1.7B-battleship-sft' --port 8000 --max-model-len 17336

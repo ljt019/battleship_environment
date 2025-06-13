d0b6f7899da275f3a618ea4d0b6157db7329f99b
Lucien Thomas <lucienthomas00@gmail.com>
2025-06-13 01:55:52 -0500

    tes

--- launch/vllm.sh
+++ launch/vllm.sh
@@ -11,9 +11,10 @@ setup_huggingface
 
 setup_dummy_openai_api_key
 
-CUDA_VISIBLE_DEVICES=0 uv run vf-vllm \
+CUDA_VISIBLE_DEVICES=0,1 uv run vf-vllm \
   --model 'ljt019/Qwen3-1.7B-battleship-grpo-beta' \
   --port 8000 \
   --host 127.0.0.1 \
   --max-model-len 22498 \
-  --tensor-parallel-size 1
+  --tensor-parallel-size 2 \
+  --gpu-memory-utilization 0.8

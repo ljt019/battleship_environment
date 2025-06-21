8adc7dc03cdebee93a9569b075fe3325a2c68ec1
Lucien Thomas <lucienthomas00@gmail.com>
2025-06-21 13:41:36 -0500

    f

--- scripts/train/train_grpo.py
+++ scripts/train/train_grpo.py
@@ -1,6 +1,10 @@
 import torch
 import verifiers as vf
 
+import os 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from battleship_grpo import BattleshipEnv
 from config import (
     MODEL_SIZE,

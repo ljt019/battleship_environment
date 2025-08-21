3596e5effcd610acb041dca8f6546bf67f372421
Lucien Thomas <lucienthomas00@gmail.com>
2025-08-21 03:12:59 -0500

    easy training scripts

--- scripts/train.py
+++ scripts/train.py
@@ -1,29 +1,20 @@
-import verifiers as vf
-
-from battleship_environment import load_environment
+import os
 
 ############## Training Config ##############
 
-MAX_TURNS = 10
-NUM_GAMES = 100
+SCRIPT_PATH = "scripts/sft.py"
 
-MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"
-
-RUN_NAME = "battleship-sft-0825"
+CUDA_VISIBLE_DEVICES = "0"
 
 #############################################
 
-env = load_environment(max_turns=MAX_TURNS, num_games=NUM_GAMES)
 
-model, tokenizer = vf.get_model_and_tokenizer(MODEL_NAME)
+def main():
+    os.system(
+        f"CUDA_VISIBLE_DEVICES={CUDA_VISIBLE_DEVICES} accelerate launch --config-file configs/zero3.yaml \
+        --num-processes {CUDA_VISIBLE_DEVICES.count(',') + 1} {SCRIPT_PATH}"
+    )
 
-args = vf.grpo_defaults(run_name=RUN_NAME)
 
-trainer = vf.GRPOTrainer(
-    model=model,
-    processing_class=tokenizer,
-    env=env,
-    args=args,
-)
-
-trainer.train()
+if __name__ == "__main__":
+    main()
---
+++ scripts/vllm.py
@@ -0,0 +1,21 @@
+import os
+
+############## vLLM Config ##############
+
+MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"
+# MODEL_NAME = "ljt019/Qwen3-4B-Instruct-bs-sft-0825" # for grpo
+
+CUDA_VISIBLE_DEVICES = "0"
+
+##############################################
+
+
+def main():
+    os.system(
+        f"CUDA_VISIBLE_DEVICES={CUDA_VISIBLE_DEVICES} uv run vf-vllm --model {MODEL_NAME} \
+        --data-parallel-size {CUDA_VISIBLE_DEVICES.count(',') + 1} --enforce-eager --disable-log-requests"
+    )
+
+
+if __name__ == "__main__":
+    main()

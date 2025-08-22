0d98cb653ac95d31343b3ec78ec1a24f7986b4c6
Lucien Thomas <lucienthomas00@gmail.com>
2025-08-22 02:52:37 -0500

    twea

---
+++ scripts/misc/test_data.py
@@ -0,0 +1,17 @@
+import verifiers as vf
+from datasets import load_dataset
+
+MODEL_NAME = "Qwen/Qwen3-4B-Instruct-2507"
+DATASET_NAME = "ljt019/battleship-multiturn-1200"
+
+tokenizer = vf.get_tokenizer(
+    MODEL_NAME,
+)
+dataset = load_dataset(DATASET_NAME, split="train")
+
+first_row = dataset[0]
+
+messages = first_row["prompt"] + first_row["completion"]
+
+toks = tokenizer.apply_chat_template(messages, tokenize=True)
+print(len(toks))
--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ snap install astral-uv --classic
 
 uv add 'verifiers[all]' && uv pip install flash-attn --no-build-isolation
 
-uv run huggingface-cli login
+uv run hf auth login
 
 uv run wandb login
